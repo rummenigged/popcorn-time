eaaac6edc07e5256dc0163900dd7b6b3f6ecd27b
rummenigged <rummenigge.duarte@gmail.com>
2022-09-15 16:17:08 -0300

    :

---
+++ app/src/test/java/com/rummenigged/popcorntime/SeriesRepositoryUnitTest.kt
@@ -0,0 +1,40 @@
+package com.rummenigged.popcorntime
+
+import com.rummenigged.popcorntime.data.repository.SeriesRepositoryImpl
+import com.rummenigged.popcorntime.domain.SeriesRepository
+import com.rummenigged.popcorntime.fakes.SeriesRemoteDataSourceFake
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+
+@ExperimentalCoroutinesApi
+class SeriesRepositoryUnitTest {
+
+    private lateinit var seriesRepository: SeriesRepository
+
+    private val listSize = 4
+
+    @Before
+    fun setup(){
+        seriesRepository = SeriesRepositoryImpl(
+            seriesRemoteDataSource = SeriesRemoteDataSourceFake(
+                seriesList = SeriesRemoteDataSourceFake.createSeriesListFakeList(listSize)
+            )
+        )
+    }
+
+    @Test
+    fun `assert get series list success `() = runTest {
+        seriesRepository.getSeriesList(1).also { result ->
+            assert(result.size == listSize)
+        }
+    }
+
+    @Test
+    fun `assert get series list success without pass page parameter`() = runTest {
+        seriesRepository.getSeriesList().also { result ->
+            assert(result.size == listSize)
+        }
+    }
+}
