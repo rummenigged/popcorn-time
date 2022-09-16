ccd2218d4f7b024af84e1e9f620d846371306c1a
rummenigged <rummenigge.duarte@gmail.com>
2022-09-16 11:30:59 -0300

    :syringe:/:wrench: Setup SeriesRemoteDataSourceImpl and SeriesRepositoryImpl  to receive dependencies through Hilt

--- app/src/main/java/com/rummenigged/popcorntime/data/repository/SeriesRepositoryImpl.kt
+++ app/src/main/java/com/rummenigged/popcorntime/data/repository/SeriesRepositoryImpl.kt
@@ -3,8 +3,9 @@ package com.rummenigged.popcorntime.data.repository
 import com.rummenigged.popcorntime.data.remoteDataSource.SeriesRemoteDataSource
 import com.rummenigged.popcorntime.domain.Series
 import com.rummenigged.popcorntime.domain.SeriesRepository
+import javax.inject.Inject
 
-class SeriesRepositoryImpl(
+class SeriesRepositoryImpl @Inject constructor(
     private val seriesRemoteDataSource: SeriesRemoteDataSource
 ): SeriesRepository {
     override suspend fun getSeriesList(page: Int?): List<Series> =
