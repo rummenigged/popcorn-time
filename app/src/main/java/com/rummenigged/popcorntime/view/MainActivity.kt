0b72f76a3ec061ce61a81407d43e63d20f319ddf
rummenigged <rummenigge.duarte@gmail.com>
2022-09-16 11:25:22 -0300

    :truck: Refactor MainActivity directory

--- app/src/main/java/com/rummenigged/popcorntime/view/MainActivity.kt
+++ app/src/main/java/com/rummenigged/popcorntime/view/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.rummenigged.popcorntime
+package com.rummenigged.popcorntime.view
 
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
@@ -9,8 +9,11 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import com.rummenigged.popcorntime.R
 import com.rummenigged.popcorntime.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
