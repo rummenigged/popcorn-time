b267233f67c333b91eaf5d89f38210493731bd15
rummenigged <rummenigge.duarte@gmail.com>
2022-09-16 11:31:13 -0300

    :newspaper: Create SeriesAdapter

---
+++ app/src/main/java/com/rummenigged/popcorntime/view/SeriesAdapter.kt
@@ -0,0 +1,37 @@
+package com.rummenigged.popcorntime.view
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.rummenigged.popcorntime.R
+import com.rummenigged.popcorntime.databinding.ItemSeriesBinding
+import com.rummenigged.popcorntime.view.common.DiffUtilBaseAdapter
+import javax.inject.Inject
+
+class SeriesAdapter @Inject constructor()
+    : DiffUtilBaseAdapter<SeriesView>(ArrayList()) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<SeriesView> =
+        when(viewType){
+            R.layout.item_series -> {
+                SeriesViewHolder(
+                    ItemSeriesBinding.inflate(
+                        LayoutInflater.from(parent.context),
+                        parent,
+                        false)
+                )
+            }
+
+            else -> throw IllegalArgumentException("Unexpected viewType received: $viewType.")
+        }
+
+    override fun getItemViewType(position: Int): Int = R.layout.item_series
+
+    inner class SeriesViewHolder(
+        private val viewBinding: ItemSeriesBinding
+    ):BaseViewHolder<SeriesView>(viewBinding) {
+        override fun bind(item: SeriesView) {
+            viewBinding.apply {
+                mtvSeriesName.text = item.name
+            }
+        }
+    }
+}
