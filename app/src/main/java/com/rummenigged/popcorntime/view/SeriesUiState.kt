70e1f8e99d2cd5ee9ba4ff2dace31a89e9591b7c
rummenigged <rummenigge.duarte@gmail.com>
2022-09-16 14:49:10 -0300

    :hammer: Refactor SeriesUiState attribute types

--- app/src/main/java/com/rummenigged/popcorntime/view/SeriesUiState.kt
+++ app/src/main/java/com/rummenigged/popcorntime/view/SeriesUiState.kt
@@ -2,9 +2,6 @@ package com.rummenigged.popcorntime.view
 
 data class SeriesUiState(
     val isLoading: Boolean = false,
-    val errorMessage: String = "",
-    val seriesList: List<SeriesView> = emptyList()
-) {
-    val hasError: Boolean
-    get() = errorMessage.isNotEmpty()
-}
+    val errorMessage: String? = null,
+    val seriesList: List<SeriesView>? = null
+)
