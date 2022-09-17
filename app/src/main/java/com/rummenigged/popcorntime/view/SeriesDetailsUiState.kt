ee4654fb10bbb3580e934a288c9955dbee872ce4
rummenigged <rummenigge.duarte@gmail.com>
2022-09-17 17:39:05 -0300

    :newspaper: Add new SeriesDetailsUiState episode list attribute

--- app/src/main/java/com/rummenigged/popcorntime/view/SeriesDetailsUiState.kt
+++ app/src/main/java/com/rummenigged/popcorntime/view/SeriesDetailsUiState.kt
@@ -4,5 +4,6 @@ data class SeriesDetailsUiState(
     val isLoading: Boolean = false,
     val errorMessage: String? = null,
     val seriesDetails: SeriesDetailsView? = null,
-    val seasonList: List<SeasonView>? = null
+    val seasonList: List<SeasonView>? = null,
+    val episodesList: List<EpisodeView>? = null
 )
