070870e63873b94c1056074cae10050859e507e5
rummenigged <rummenigge.duarte@gmail.com>
2022-09-17 19:34:00 -0300

    :hammer: Add new attributes in SeriesDetailsUiState

--- app/src/main/java/com/rummenigged/popcorntime/view/SeriesDetailsUiState.kt
+++ app/src/main/java/com/rummenigged/popcorntime/view/SeriesDetailsUiState.kt
@@ -2,7 +2,9 @@ package com.rummenigged.popcorntime.view
 
 data class SeriesDetailsUiState(
     val isLoading: Boolean = false,
+    val isLoadingEpisodes: Boolean = false,
     val errorMessage: String? = null,
+    val errorEpisodeList: String? = null,
     val seriesDetails: SeriesDetailsView? = null,
     val seasonList: List<SeasonView>? = null,
     val episodesList: List<EpisodeView>? = null
