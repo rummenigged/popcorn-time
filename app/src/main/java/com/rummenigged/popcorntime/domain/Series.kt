fc92b09b79eb2c8f6ecb15e40064627d7681947c
rummenigged <rummenigge.duarte@gmail.com>
2022-09-17 14:53:46 -0300

    :hammer: Add new Series attributes

--- app/src/main/java/com/rummenigged/popcorntime/domain/Series.kt
+++ app/src/main/java/com/rummenigged/popcorntime/domain/Series.kt
@@ -4,5 +4,14 @@ data class Series(
     val id: Int,
     val url: String,
     val name: String,
-    val imageUrl: String
-)
+    val imageUrl: String,
+    val posterUrl: String,
+    val schedule: Schedule,
+    val summary: String,
+    val genres: List<String>
+){
+    data class Schedule(
+        val time: String,
+        val days: List<String>
+    )
+}
