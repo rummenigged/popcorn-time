c8a026054be69a776801ad19c5cf41b0af7febfb
rummenigged <rummenigge.duarte@gmail.com>
2022-09-17 16:39:49 -0300

    :hammer: Add seasonList in SeriesDetailsUiState

--- app/src/main/java/com/rummenigged/popcorntime/view/SeriesDetailsUiState.kt
+++ app/src/main/java/com/rummenigged/popcorntime/view/SeriesDetailsUiState.kt
@@ -3,5 +3,6 @@ package com.rummenigged.popcorntime.view
 data class SeriesDetailsUiState(
     val isLoading: Boolean = false,
     val errorMessage: String? = null,
-    val seriesDetails: SeriesDetailsView? = null
+    val seriesDetails: SeriesDetailsView? = null,
+    val seasonList: List<SeasonView>? = null
 )
